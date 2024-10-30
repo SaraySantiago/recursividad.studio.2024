407a4739641e2ff0a0bcaf9e938657a64fdd6c70
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-30 11:56:39 +0100

    .

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
 
-#from factorialIterativo import factorial #Va más deprisa que la recursiva
-from factorialRecursivo import factorial
+from factorialIterativo import factorial #Va más deprisa que la recursiva
+#from factorialRecursivo import factorial
 if __name__ == "__main__":
     
     for i in range(500):
         factorial (i)
      #print(i, factorial(i))
      #print() #salto de linea
+     
+     
+     
+     
