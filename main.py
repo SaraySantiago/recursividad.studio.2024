7e508cf8a7a7e5326c42a5af21958790730e6ece
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-30 11:04:48 +0100

    .

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 #C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
 
-#from factorialIterativo import factorial 
+#from factorialIterativo import factorial #Va más deprisa que la recursiva
 from factorialRecursivo import factorial
-
-def factorial(n):
-    """Factorial recursivo"""
-    if n == 0: return 1 
-    if n == 1: return 1 
-    return n * factorial (n-1)
-     
-
 if __name__ == "__main__":
     
     for i in range(500):
-     print(i, factorial(i))
-     print() #salto de linea
+        factorial (i)
+     #print(i, factorial(i))
+     #print() #salto de linea
