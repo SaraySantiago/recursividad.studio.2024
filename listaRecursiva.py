b9dfdc3f715fdb6d65adac3df4e64222fa069ad4
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-30 12:25:59 +0100

    .

---
+++ listaRecursiva.py
@@ -0,0 +1,29 @@
+#C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+def imprime(lista):
+    """impresión recursiva de una lista
+        con enfoque primero; restantes"""
+        
+    print("<", end=" ")
+    if len(lista) == 0: 
+        print(">", end=" ")
+        return 
+    primero = lista[0]
+    listaRestantes = lista[1:]
+    print(primero, end= ", ")
+    imprime(listaRestantes)
+    print(">", end=" ")
+
+if __name__ == "__main__":
+    
+    l=[33,22,11]
+    imprime(l)
+    
+    l=[33,22]
+    imprime(l)
+    
+    l=[33]
+    imprime(l)
+    
+    l=[]
+    imprime(l)
