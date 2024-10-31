b47df69225e51ff450116b55faeadaafc7abef51
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-31 12:26:10 +0100

    .

--- listaRecursiva.py
+++ listaRecursiva.py
@@ -3,27 +3,26 @@
 def imprime(lista):
     """impresión recursiva de una lista
         con enfoque primero; restantes"""
-        
-    print("<", end=" ")
+    #casos triviales ...
+    print("( ", end=" ")
     if len(lista) == 0: 
-        print(">", end=" ")
+        print(")", end=" ")
         return 
-    primero = lista[0]
-    listaRestantes = lista[1:]
-    print(primero, end= ", ")
-    imprime(listaRestantes)
-    print(">", end=" ")
+    print(lista[0], end= ", ") #...primero.
+    imprime(lista[1:]) #Restantes: RECURSIÓN
+    print(")", end=" ")
 
 if __name__ == "__main__":
     
     l=[33,22,11]
     imprime(l)
-    
+    print() #salto de linea
     l=[33,22]
     imprime(l)
-    
+    print() #salto de linea
     l=[33]
     imprime(l)
-    
+    print() #salto de linea
     l=[]
     imprime(l)
+    print() #salto de linea
